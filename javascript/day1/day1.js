9f024c32f71359ee0991c028aa3afe078e7b71d9
Toby Curtis <toby.curtis@revcult.com>
2023-12-01 11:02:21 -0800

    rawr

--- javascript/day1/day1.js
+++ javascript/day1/day1.js
@@ -10,11 +10,11 @@ rl.on('line', (consoleInput) => {
   if (consoleInput === 'end') {
     rl.close()
   } else {
-    const nums = Array.from(consoleInput).filter(o => o.charCodeAt() >= 49 && o.charCodeAt() <= 57)
+		const nums = Array.from(consoleInput).filter(o => o.charCodeAt() >= 49 && o.charCodeAt() <= 57)
 		const first = nums.shift()
 		calibrationValues.push(`${first}${nums.length ? nums.pop() : first}`)
   }
 }).on('close', () => {
 	console.log('calibrationValues sum', calibrationValues.reduce((m, c) => m += Number(c), 0))
-  process.exit(0);
+	process.exit(0);
 });
