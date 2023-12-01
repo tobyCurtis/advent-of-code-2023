5bdc7b0dd3a2bff6d1930ca093015b7de30c2966
Toby Curtis <toby.curtis@revcult.com>
2023-12-01 11:29:40 -0800

    a

--- javascript/day1/day1.js
+++ javascript/day1/day1.js
@@ -10,7 +10,7 @@ rl.on('line', (consoleInput) => {
   if (consoleInput === 'end') {
     rl.close()
   } else {
-    const nums = Array.from(consoleInput).filter(o => o.charCodeAt() >= 49 && o.charCodeAt() <= 57)
+    const nums = [...consoleInput].filter(o => o.charCodeAt() >= 49 && o.charCodeAt() <= 57)
     const first = nums.shift()
     calibrationValues.push(`${first}${nums.length ? nums.pop() : first}`)
   }
